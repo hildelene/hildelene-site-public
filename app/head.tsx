136bb35db82748b78e7b216f25817b3cc79d7515
VFL15 <seu-email@example.com>
2026-04-17 18:44:39 -0300

    Ajusta favicons, manifest.json e ícones para compatibilidade total PWA e navegador

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,7 @@ export default function Head() {
     <>
       <title>UERJ Hildelene Nautidesign</title>
       <meta name="description" content="Pioneirismo no futuro da engenharia naval com inovacao, precisao e excelencia tecnologica. A Hildelene Nautidesign e uma equipe dedicada a superar limites na tecnologia maritima." />
+      <link rel="icon" type="image/svg+xml" href="/midia/logo_hildelene_vetor_cmyk.svg" />
       <link rel="icon" type="image/png" href="/midia/logo_hildelene.png" />
       <link rel="apple-touch-icon" href="/midia/logo_hildelene.png" />
       <link rel="manifest" href="/manifest.json" />
