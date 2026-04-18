f43e9cc9ef69436e8d7e5056c0f1362ad9e588c7
VFL15 <seu-email@example.com>
2026-04-17 21:48:52 -0300

    Commit e push de alterações adicionais

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>UERJ Hildelene Nautidesign</title>
       <meta name="description" content="Equipe universitária de nautimodelismo da UERJ." />
-      <link rel="icon" href="/midia/logo_hildelene_vetor_cmyk.svg" type="image/svg+xml" />
+      <link rel="icon" type="image/png" href="/midia/logo_hildelene.png" />
       <link rel="manifest" href="/manifest.json" />
       {/* outros links/metas */}
     </>
