e0ba49973d4a22486de861c8162d3d47bd0135a5
VFL15 <seu-email@example.com>
2026-04-17 21:16:23 -0300

    Ajusta favicon para SVG no head e PNG apenas no manifest.json (PWA Android)

--- app/head.tsx
+++ app/head.tsx
@@ -2,10 +2,8 @@ export default function Head() {
   return (
     <>
       <title>UERJ Hildelene Nautidesign</title>
-      <meta name="description" content="Pioneirismo no futuro da engenharia naval com inovacao, precisao e excelencia tecnologica. A Hildelene Nautidesign e uma equipe dedicada a superar limites na tecnologia maritima." />
+      <meta name="description" content="Equipe universitária de nautimodelismo da UERJ." />
       <link rel="icon" type="image/svg+xml" href="/midia/logo_hildelene_vetor_cmyk.svg" />
-      <link rel="icon" type="image/png" href="/midia/logo_hildelene.png" />
-      <link rel="apple-touch-icon" href="/midia/logo_hildelene.png" />
       <link rel="manifest" href="/manifest.json" />
       {/* Adicione outros links/metas se quiser */}
     </>
