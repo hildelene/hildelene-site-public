d6f4c133f13e68e91f98b1493355b5bef4769ecf
VFL15 <seu-email@example.com>
2026-04-17 21:51:41 -0300

    Remove todos os links de imagem do head.tsx para teste de origem do favicon

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,6 @@ export default function Head() {
     <>
       <title>UERJ Hildelene Nautidesign</title>
       <meta name="description" content="Equipe universitária de nautimodelismo da UERJ." />
-      <link rel="icon" type="image/png" href="/midia/logo_hildelene.png" />
       <link rel="manifest" href="/manifest.json" />
       {/* outros links/metas */}
     </>
